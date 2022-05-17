1fdbfa461da569d471e365349308fd70c5d2c155
Rafael Dietrich <rafaeldm@uber.com>
2022-05-16 17:56:46 -0700

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,72 @@
+vim.notify('Hello from the other side init.lua')
+
+require('packer').startup(function() 
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'mhartington/oceanic-next'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+end)
+
+vim.o.number = true
+vim.o.relativenumber = true
+-- TODO: change this to jk and don't be a degenerate like niels
+vim.keymap.set("i", "jk", "<esc>")
+
+local lsp = require("lspconfig")
+local cmp = require("cmp")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+local on_attach = function(client, bufnr)
+	local opts = { noremap = true,
+	               silent  = true,
+	               buffer = bufnr }
+
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+end
+
+local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+lsp.gopls.setup({
+	name = "gopls",
+	cmd = {"gopls", "--remote=auto", "serve"},
+	init_options = {
+		gofumpt = true,
+		staticcheck = true,
+		memoryMode = "DegradeClosed"
+	},
+	whitelist = {"go"},
+	capabilities = capabilities,
+	on_attach = on_attach
+})
+
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+cmp.setup({
+	preselect = cmp.PreselectMode.None,
+	mapping = cmp.mapping.preset.insert({
+              ['<c-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<c-f>'] = cmp.mapping.scroll_docs(4),
+              ['<tab>'] = cmp.mapping.select_next_item(),
+              ['<s-tab>'] = cmp.mapping.select_prev_item(),
+              ['<c-space>'] = cmp.mapping.complete(),
+              ['<c-e>'] = cmp.mapping.abort(),
+              ['<cr>'] = cmp.mapping.confirm({select  = true})
+        }),
+        sources = cmp.config.sources({
+              {name = "nvim_lsp"}
+	})
+})
+
+vim.cmd [[
+  set termguicolors
+  syntax enable
+  colorscheme OceanicNext
+]]
--- zshrc
+++ zshrc
@@ -1,5 +1,6 @@
 export PATH="/usr/local/opt/php@7.3/bin:$PATH"
 export PATH="/usr/local/opt/php@7.3/sbin:$PATH"
+export PATH="/Users/rafaeldm/go/bin:$PATH"
 
 echo -e "`date +"%Y-%m-%d %H:%M:%S"` direnv hooking zsh"
 eval "$(direnv hook zsh)"
