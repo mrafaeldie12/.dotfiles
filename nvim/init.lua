39c5a7d40a58fc16223c0f91cbe44ebe114887ee
Rafael Dietrich <mrafael.dietrich@gmail.com>
2022-05-31 21:42:01 -0700

    Add go to type definition

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,6 +43,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 end
 
